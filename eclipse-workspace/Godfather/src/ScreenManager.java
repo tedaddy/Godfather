c941c265954e9f7f99449e3e14155c89c591b86e
tedaddy <sereshka0967@gmail.com>
2020-01-06 21:55:31 +0300

    update class ScreenManager

--- eclipse-workspace/Godfather/src/ScreenManager.java
+++ eclipse-workspace/Godfather/src/ScreenManager.java
@@ -1,4 +1,9 @@
 import java.awt.CardLayout;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.*;
 
@@ -6,10 +11,30 @@ public class ScreenManager {
 	Frame frame;
 	JPanel contentPane;
 	
-	public ScreenManager() {
+	String ABSOLUTE_PATH;
+	
+	public ScreenManager(String path) {
+		this.ABSOLUTE_PATH = path;
+		
 		frame = new Frame();
 		contentPane = new JPanel();
+		frame.setContentPane(contentPane);
 		contentPane.setLayout(new CardLayout());
+		
+		try {
+		     GraphicsEnvironment ge = 
+		         GraphicsEnvironment.getLocalGraphicsEnvironment();
+		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(ABSOLUTE_PATH + "/fonts/HollyWoodHills.ttf")));
+		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(ABSOLUTE_PATH + "/fonts/OCR.TTF")));
+		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(ABSOLUTE_PATH + "/fonts/consolas.ttf")));
+		} catch (IOException|FontFormatException e) {
+		     System.out.println(e);
+		}
+		
+		MainPage mainPage = new MainPage(frame.width, frame.height, this);
+		
+		contentPane.add(mainPage, "main page");
+		
 	}
 	
 	public void getPage(String pageName) {
