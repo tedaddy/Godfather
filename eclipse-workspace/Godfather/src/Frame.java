d3347f53dc9e9da01ae2b66167359f10642aeb09
tedaddy <sereshka0967@gmail.com>
2020-01-07 17:51:39 +0300

    move var absolute_path from Main to Frame

--- eclipse-workspace/Godfather/src/Frame.java
+++ eclipse-workspace/Godfather/src/Frame.java
@@ -1,17 +1,37 @@
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import javax.swing.*;
 
 public class Frame extends JFrame{
+	String absolute_path;
 	
 	static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	static int width = Double.valueOf(screenSize.getWidth()).intValue();
 	static int height = Double.valueOf(screenSize.getHeight()).intValue();
 	
 	public Frame() {
+		Path currentRelativePath = Paths.get("");
+		String path = currentRelativePath.toAbsolutePath().toString();
+		absolute_path = path.replaceAll("\\\\", "/");
+		try {
+		     GraphicsEnvironment ge = 
+		         GraphicsEnvironment.getLocalGraphicsEnvironment();
+		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(absolute_path + "/fonts/HollyWoodHills.ttf")));
+		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(absolute_path + "/fonts/OCR.TTF")));
+		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(absolute_path + "/fonts/consolas.ttf")));
+		} catch (IOException|FontFormatException e) {
+		     System.out.println(e);
+		}
+		
+		addKeyListener(new ExitKeyListener());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setUndecorated(true);
 		setResizable(true);
--- eclipse-workspace/Godfather/src/Main.java
+++ eclipse-workspace/Godfather/src/Main.java
@@ -2,17 +2,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-	static ScreenManager screenManager;
+	static ScreenManager screenManager = new ScreenManager();
 
 	public static void main(String[] args) {
-		Path currentRelativePath = Paths.get("");
-		String path = currentRelativePath.toAbsolutePath().toString();
-		path = path.replaceAll("\\\\", "/");
-		
-		screenManager = new ScreenManager(path);
-		
-		screenManager.getPage("main page");
-
+		screenManager.showPage("home");
 	}
 
 }
